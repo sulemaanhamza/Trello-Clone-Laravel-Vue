b139092ee738b6a19b65edd679920ca7bbcdff18
Suleman <sulemanhamza97@gmail.com>
2022-08-04 21:22:08 +0500

    Implemented vue draggable

--- resources/js/components/board.vue
+++ resources/js/components/board.vue
@@ -5,30 +5,44 @@
             <div class='layout__list'>
                 <div class="list__header">
                     <p><strong>Column 1</strong></p>
+                    <a href="javascript:0">Delete</a>
                 </div>
-
-                <div class='list__card'>
-                    <label class='card__label'>Trello board mockup</label>
-                    <!-- <div class='card__date'>August 4th</div> -->
-                </div>
-                <div class='list__card'>
-                    <label>Test card by Suleman</label>
-                </div>
+                <draggable v-model="myArray" group="people" @start="drag=true" @end="drag=false">
+                    <div class='list__card'>
+                        <label class='card__label'>Trello board mockup</label>
+                        <!-- <div class='card__date'>August 4th</div> -->
+                    </div>
+                    <div class='list__card'>
+                        <label>Test card by Suleman</label>
+                    </div>
+                </draggable>
             </div>
 
             <div class='layout__list'>
-                <div class='list__card'>
-                    <label>Lorem ipsum dolor sit amet..</label>
+                <div class="list__header">
+                    <p><strong>Column 2</strong></p>
+                    <a href="javascript:0">Delete</a>
                 </div>
+                <draggable v-model="myArray" group="people" @start="drag=true" @end="drag=false">
+                    <div class='list__card'>
+                        <label>Lorem ipsum dolor sit amet..</label>
+                    </div>
+                </draggable>
             </div>
 
             <div class='layout__list'>
-                <div class='list__card'>
-                    <label>Lorem ipsum dolor sit amet consectetur.</label>
+                <div class="list__header">
+                    <p><strong>Column 3</strong></p>
+                    <a href="javascript:0">Delete</a>
                 </div>
-                <div class='list__card'>
-                    <label>Lorem ipsum dolor sit amet consectetur.</label>
-                </div>
+                <draggable v-model="myArray" group="people" @start="drag=true" @end="drag=false">
+                    <div class='list__card'>
+                        <label>Lorem ipsum dolor sit amet consectetur.</label>
+                    </div>
+                    <div class='list__card'>
+                        <label>Lorem ipsum dolor sit amet consectetur.</label>
+                    </div>
+                </draggable>
             </div>
 
         </div>
@@ -36,8 +50,13 @@
 </template>
 
 <script>
+import draggable from 'vuedraggable';
+
 export default {
 
+    components: {
+            draggable,
+    },
 }
 </script>
 
